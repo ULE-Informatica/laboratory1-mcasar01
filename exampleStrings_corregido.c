174079e91c11211ba48d403ae68afbeffe251cb7
MCPS10 <sf10710@gmail.com>
2020-11-14 19:18:28 +0100

    Código corregido respecto a c99

---
+++ exampleStrings_corregido.c
@@ -0,0 +1,99 @@
+/*CÓDIGO CORREGIDO RESPECTO AL ESTANDAR C99*/
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+char array1[] = "Foo" "bar"; 
+char array2[] = { 'F', 'o', 'o', 'b', 'a', 'r', '\0' };
+ 
+enum { BUFFER_MAX_SIZE = 1024 };
+ 
+const char* s1 = R"foo(
+Hello
+  World
+)foo";
+const char* s2 = "\nHello\nWorld\n";
+
+void gets_example_func(void) {
+  char buf[BUFFER_MAX_SIZE];
+ 
+  if (fgets(buf, sizeof(buf), stdin) == NULL) {
+        exit(0);
+  }
+  buf[strlen(buf) - 1] = '\0';
+}
+
+const char *get_dirname(const char *pathname) {
+  char *slash;
+  slash = strrchr(pathname, '/');
+  if (slash) {
+    *slash = '\0'; /* Undefined behavior */
+  }
+  return pathname;
+}
+ 
+
+void get_y_or_n(void) {  
+	char response[1];
+
+	printf("Continue? [y] n: ");  
+	fgets(response,sizeof(response),stdin);
+
+	if (response[0] == 'n') 
+		exit(0);  
+
+	return;
+}
+
+ 
+int main(int argc, char *argv[])
+{
+    char key[24];
+    char response[1];
+    char array3[16];
+    char array4[16];
+    char array5 []  = "01234567890123456";
+    char *ptr_char  = "new string literal";
+    int size_array1 = strlen("аналитик");
+    int size_array2 = 100;
+   // char analitic1[size_array1]="аналитик";
+   // char analitic2[size_array2]="аналитик";
+    char analitic3[100]="аналитик";
+
+    puts(get_dirname(__FILE__));
+
+        
+    strcpy(key, argv[1]);  
+    strcat(key, " = ");  
+    strcat(key, argv[2]);
+
+
+    fgets(response,sizeof(response),stdin);
+    
+    get_y_or_n();
+
+    printf ("%d",size_array1);
+    printf ("\n");
+    printf ("%d",size_array2);
+    printf ("\n");
+ 
+    puts (s1);
+    printf ("\n");
+    puts (s2);
+    printf ("\n");
+    
+    strncpy(array3, array5, sizeof(array3));  
+    strncpy(array4, array3, strlen(array3));
+    
+    array5 [0] = 'M';
+    ptr_char [0] = 'N';
+    
+    array3[sizeof(array3)-1]='\0';
+    
+    
+    return 0;
+}
+
